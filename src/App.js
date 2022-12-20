93713e5fc3c8124258c1fd77a0c3114197a38631
Tiago Araujo <tiagoaraujosud@gmail.com>
2022-12-20 11:47:54 -0300

    Usestate

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 
+import { useState } from 'react';
 import {FiSearch} from 'react-icons/fi';
 import './styles.css';
 
 
 function App() {
+
+  const[input, setInput] = useState('')
+
+  function handleSearch(){
+    alert("Valor do input " + input)
+  }
+
   return (
     <div className="container">
       <h1 className="title">Zip Code Finder</h1>
 
       <div className="containerInput">
-        <input type="text" placeholder="Type your zip code"/>
+        <input type="text" 
+        placeholder="Type your zip code" 
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        />
         
-        <button className="buttonSearch">
+        <button className="buttonSearch" onClick={handleSearch}>
           <FiSearch size={25} color="#FFF"/>
         </button>
       </div>
